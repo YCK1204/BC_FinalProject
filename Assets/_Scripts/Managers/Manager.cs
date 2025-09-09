c63aa060a2aa3f883e9f9066104ade2340c694d6
YCK1204 <dpcks5959@naver.com>
2025-09-09 23:08:33 +0900

    [feat] PoolManager

--- Assets/_Scripts/Managers/Manager.cs
+++ Assets/_Scripts/Managers/Manager.cs
@@ -40,4 +40,6 @@ public class Manager : MonoBehaviour
     public static SceneManagerEx Scene { get { return Instance._scene; } set { Instance._scene = value; value.transform.parent = _instance.transform; } }
     AudioManager _audio;
     public static AudioManager Audio { get { return Instance._audio; } set { Instance._audio = value; value.transform.parent = _instance.transform; } }
+    PoolManager _pool;
+    public static PoolManager Pool { get { return Instance._pool; } set { Instance._pool = value; value.transform.parent = _instance.transform; } }
 }
