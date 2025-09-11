0032aa1ccb9ff53f44f84dd238a8045be7deb99c
KimNamJin <48212042+UserJin@users.noreply.github.com>
2025-09-11 16:24:48 +0900

    [chore] 폴더 구조 정리

--- Assets/_Scripts/Monster/Implementation/Move/ChaseMove.cs
+++ Assets/_Scripts/Monster/Implementation/Move/ChaseMove.cs
@@ -1,32 +1,47 @@
 using UnityEngine;
 
-public class PatrolMove : Game.Monster.IMovable
+/// <summary>
+/// 대상을 추적하는 이동방법
+/// </summary>
+public class ChaseMove : Game.Monster.IMovable
 {
     private float _speed;
     Transform _tr;
     Rigidbody2D _rb;
     Collider2D _col;
+    Transform _target;
 
-    // 레이어 마스크 그라운드
-
-    public PatrolMove(float speed, Transform tr, Rigidbody2D rb, Collider2D col)
+    public ChaseMove(float speed, Transform tr, Rigidbody2D rb, Collider2D col, Transform target)
     {
         _speed = speed;
         _tr = tr;
         _rb = rb;
         _col = col;
+        _target = target;
     }
 
     public void Move()
     {
-        _rb.linearVelocityX = _speed * _tr.localScale.x;
+        if (_target == null)
+            return;
+
+        if (Mathf.Abs(_target.position.x - _tr.position.x) < 0.1f)
+        {
+            StopMove();
+            return;
+        }
+
+        int dir = _target.position.x - _tr.position.x > 0 ? 1 : -1;
+
+        _rb.linearVelocityX = _speed * dir;
+
         RaycastHit2D floor = Physics2D.Raycast(_tr.position + new Vector3(_col.bounds.size.x / 2f + 0.1f, 0, 0) * _tr.localScale.x, Vector2.down, (_col.bounds.size.y / 2f + 0.1f));
         Debug.DrawRay(_tr.position + new Vector3(_col.bounds.size.x / 2f + 0.1f, 0, 0) * _tr.localScale.x, Vector2.down * (_col.bounds.size.y / 2f + 0.1f), Color.red);
         RaycastHit2D wall = Physics2D.Raycast(_tr.position + new Vector3(_col.bounds.size.x / 2f + 0.1f, 0, 0) * _tr.localScale.x, Vector2.right * _tr.localScale.x, 0.3f);
         Debug.DrawRay(_tr.position + new Vector3(_col.bounds.size.x / 2f + 0.1f, 0, 0) * _tr.localScale.x, Vector2.right * _tr.localScale.x * 0.3f, Color.blue);
 
-        if(floor.collider == null || wall.collider != null)
-            _tr.localScale = new Vector3( -1 * _tr.localScale.x, _tr.localScale.y, _tr.localScale.z);
+        if (floor.collider == null || wall.collider != null)
+            StopMove();
     }
 
     public void StopMove()
--- Assets/_Scripts/Monster/State/MonsterState/MonsterAttackState.cs
+++ Assets/_Scripts/Monster/State/MonsterState/MonsterAttackState.cs
@@ -6,4 +6,6 @@ public class MonsterAttackState : MonsterBaseState
     {
         StateType = Common.StateType.Attack;
     }
+
+
 }
