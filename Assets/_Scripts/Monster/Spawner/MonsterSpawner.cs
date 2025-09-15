815ac5e46af5f35f5f1766a031cf48a409687bcb
YCK1204 <dpcks5959@naver.com>
2025-09-15 14:19:26 +0900

    [feat] 몬스터 처치 콜백 처리

--- Assets/_Scripts/Monster/Spawner/MonsterSpawner.cs
+++ Assets/_Scripts/Monster/Spawner/MonsterSpawner.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System;
 using System.Collections.Generic;
 using Object = UnityEngine.Object;
+using System.Collections;
 
 // 몬스터 스폰 (프리팹, 위치)
 // 스폰 조건 (게임 시작 즉시, 다른 스폰 리스트를 다잡고, 플레이어가 특정 범위 내에 접근 시)
@@ -42,18 +43,50 @@ public abstract class MonsterSpawner : MonoBehaviour
             }
         }
     }
-
+    List<Monster> _spawnedMonsters = new List<Monster>();
+    private void Start()
+    {
+        StartCoroutine(LateStart(() =>
+        {
+            Manager.Game.MonsterCount += SpawnInfos.Count;
+        }));
+    }
+    IEnumerator LateStart(Action action)
+    {
+        yield return null;
+        action.Invoke();
+    }
     public void SpawnAll()
     {
         if (_isSpawned) return;
 
         foreach (var info in SpawnInfos)
         {
-            var go = Instantiate(info.Prefab);
-            if (Root != null) go.transform.parent = Root;
-            go.transform.position = info.Position;
+            var monster = Manager.Resource.Instantiate<Monster>(info.Prefab);
+            monster.OnDied += () =>
+            {
+                SpawnedCount--;
+                Manager.Game.MonsterCount--;
+            };
+            if (Root != null) monster.transform.parent = Root;
+            monster.transform.position = info.Position;
+            _spawnedMonsters.Add(monster);
             // 몬스터 OnDestroy 시 SpawnedCount 감소
         }
+        SpawnedCount = SpawnInfos.Count;
         _isSpawned = true;
+    }
+    [SerializeField]
+    string key;
+    private void Update()
+    {
+        if (Input.GetKey(key))
+        {
+            foreach (var m in _spawnedMonsters)
+            {
+                if (m != null)
+                    m.Die();
+            }
+        }
     }
 }
