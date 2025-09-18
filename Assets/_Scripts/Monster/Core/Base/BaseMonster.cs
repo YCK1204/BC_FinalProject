b871c03e8ae0c93e7a95112ad0fcb1edc28c7202
KimNamJin <48212042+UserJin@users.noreply.github.com>
2025-09-18 21:01:14 +0900

    [feat] 슈퍼아머 기능 추가
    - 아직은 stun 애니메이션이 없어서 상태 변화는 따로 적용하지 않음

--- Assets/_Scripts/Monster/Core/Base/BaseMonster.cs
+++ Assets/_Scripts/Monster/Core/Base/BaseMonster.cs
@@ -43,6 +43,10 @@ public abstract class BaseMonster : MonoBehaviour, Game.Monster.IDamageable
 
     public Action OnDied;
     public Action OnUpdate;
+    public Action OnHit;
+
+    // 슈퍼아머 상태 확인 변수
+    public bool IsSuperArmor = false;
 
     protected virtual void Awake()
     {
@@ -118,12 +122,13 @@ public abstract class BaseMonster : MonoBehaviour, Game.Monster.IDamageable
     public virtual void TakeDamage(int damage)
     {
         _dataHandler.TakeDamage(damage);
+        OnHit?.Invoke();
 
         if (_dataHandler.CurHp <= 0)
         {
             _stateMachine.ChangeState(Game.Monster.StateType.Die);
         }
-        else
+        else if(!IsSuperArmor)
         {
             _stateMachine.ChangeState(Game.Monster.StateType.Hit);
         }
--- Assets/_Scripts/Monster/SpecialAbilllity/SuperArmor.cs
+++ Assets/_Scripts/Monster/SpecialAbilllity/SuperArmor.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 
-public class SuperArmor : Game.Monster.ISpecialAbillity
+public class SuperArmor : MonoBehaviour, Game.Monster.ISpecialAbillity
 {
     public void Init(BaseMonster monster)
     {
-        
+        monster.IsSuperArmor = true;
+        // Todo: 특수 효과 추가?
+
+        monster.OnHit += () =>
+        {
+            if (monster.MonsterData.CurHp <= monster.MonsterData.Data.MaxHp * 0.5f)
+            {
+                monster.IsSuperArmor = false;
+                // 상태 전환 - 스턴
+            }
+        };
     }
 }
