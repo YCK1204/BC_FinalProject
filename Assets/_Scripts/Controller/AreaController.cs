59abec079911c5c9f6ae7659e991cba3e1da7fbc
YCK1204 <dpcks5959@naver.com>
2025-10-04 01:56:14 +0900

    [feat] 영역 이펙트 자동화

--- Assets/_Scripts/Controller/AreaController.cs
+++ Assets/_Scripts/Controller/AreaController.cs
@@ -3,18 +3,25 @@ using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AreaController : MonoBehaviour
 {
     ItemAreaData _data;
     Animator _animator;
-    private void Start()
+    SpriteRenderer _spriteRenderer;
+    bool _isSetComponent = false;
+    void SetPomponent()
     {
-        _animator = GetComponent<Animator>(); // 나중에 Area 데이터에 애니메이터 경로 넣고 설정해야함
+        _spriteRenderer = GetComponent<SpriteRenderer>(); // 나중에 투사체 이미지 따로 저장해야함
+        _animator = GetComponent<Animator>();
+        _isSetComponent = true;
     }
     public void Init(ItemAreaData data, PlayerCharacter owner)
     {
+        if (!_isSetComponent)
+            SetPomponent();
         _data = data;
         _animator.runtimeAnimatorController = _data.Animator;
 
@@ -30,19 +37,53 @@ public class AreaController : MonoBehaviour
                 transform.position = GetInRangeEnemyPosition(owner).position;
                 break;
         }
+
+        SetAnimEvent();
+    }
+    void SetAnimEvent()
+    {
+        var clips = _animator.runtimeAnimatorController.animationClips;
+        foreach (var clip in clips)
+        {
+            bool modified = false;
+            var events = clip.events;
+            foreach (var e in events)
+            {
+                if (string.IsNullOrEmpty(e.functionName))
+                {
+                    e.functionName = "HitEnemiesInRadius";
+                    modified = true;
+                }
+            }
+            if (modified)
+            {
+                clip.events = events;
+            }
+        }
     }
     Transform GetNearestEnemyPosition(PlayerCharacter player)
     {
-        var hits = Physics2D.OverlapCircleAll(transform.position, _data.Radius, LayerMask.GetMask("Monster"));
+        var hits = Physics2D.OverlapCircleAll(player.transform.position, _data.Radius, LayerMask.GetMask("Monster"));
         if (hits.Length != 0)
-            return hits.OrderBy(x => player.transform.DirectionTo(x.transform)).First().transform;
+        {
+            try
+            {
+                return hits.Where(x => x != null).OrderBy(x => Vector2.Distance(player.transform.position, x.transform.position)).First().transform;
+            }
+            catch (System.Exception e)
+            {
+            }
+        }
         return player.transform;
     }
     Transform GetInRangeEnemyPosition(PlayerCharacter player)
     {
-        var hits = Physics2D.OverlapCircleAll(transform.position, _data.Radius, LayerMask.GetMask("Monster"));
+        var hits = Physics2D.OverlapCircleAll(player.transform.position, _data.Radius, LayerMask.GetMask("Monster"));
         if (hits.Length != 0)
+        {
+            hits = hits.Where(x => x != null).ToArray();
             return hits[Random.Range(0, hits.Length)].transform;
+        }
         return player.transform;
     }
     IEnumerator CoShot()
@@ -62,7 +103,7 @@ public class AreaController : MonoBehaviour
         foreach (var hit in hits)
         {
             var monster = hit.GetComponent<NormalMonster>();
-            //monster.TakeDamage((int)_data.Damage);
+            monster.TakeDamage(0);
         }
     }
 }
