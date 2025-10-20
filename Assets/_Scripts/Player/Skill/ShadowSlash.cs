05876d3cace004b51019dd5d7bbad2047bfe54c6
wooyoung-1 <wooyoung7749@gmail.com>
2025-10-20 21:06:49 +0900

    [fix] 스킬작업

--- Assets/_Scripts/Player/Skill/ShadowSlash.cs
+++ Assets/_Scripts/Player/Skill/ShadowSlash.cs
@@ -1,29 +1,40 @@
 using UnityEngine;
 using Game.Player;
+using System.Collections;
 
 public class ShadowSlash : Skill
 {
     [SerializeField] private GameObject _projectilePrefab;
     [SerializeField] private Vector2 _spawnOffset = new Vector2(0.8f, 0.5f);
+    [SerializeField] private int _count = 5;
+    [SerializeField] private float _delay = 0.10f;
 
     public override void Execute()
     {
-        //if (_projectilePrefab == null)
-        //{
-        //    Debug.LogError("cc", this.gameObject);
-        //    return;
-        //}
+        StartCoroutine(ShootProjectilesDelay());
+    }
 
+    private IEnumerator ShootProjectilesDelay()
+    {
         float facingSign = owner.StateMachine.FacingSign;
         Vector2 offset = new Vector2(_spawnOffset.x * facingSign, _spawnOffset.y);
         Vector2 spawnPosition = (Vector2)owner.transform.position + offset;
-        Quaternion rotation = facingSign > 0 ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
 
-        GameObject projectileObj = PlayerPool.Instance.GetFromPool(_projectilePrefab, spawnPosition, rotation);
-
-        if (projectileObj.TryGetComponent(out ShadowSlashProjectile projectileInstance))
+        for (int i = 0; i < _count; i++)
         {
-            projectileInstance.Launch(owner);
+            Quaternion rotation = facingSign > 0 ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
+
+            GameObject projectileObj = PlayerPool.Instance.GetFromPool(_projectilePrefab, spawnPosition, rotation);
+
+            if (projectileObj.TryGetComponent(out ShadowSlashProjectile projectileInstance))
+            {
+                projectileInstance.Launch(owner);
+            }
+
+            if (i < _count - 1)
+            {
+                yield return new WaitForSeconds(_delay);
+            }
         }
     }
 }
