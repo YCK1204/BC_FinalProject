c9232af76235421bf434e2fb35ca7257a1ef9868
wooyoung-1 <wooyoung7749@gmail.com>
2025-10-28 09:09:00 +0900

    [fix] 주석추가

--- Assets/_Scripts/Managers/DisplayManager.cs
+++ Assets/_Scripts/Managers/DisplayManager.cs
@@ -24,6 +24,8 @@ public class DisplayManager : MonoBehaviour
         Instance = this;
     }
 
+
+    //픽셀 퍼펙트 카메라 키고 끄기 SetPPC(true) 카메라 연출 사용전에 켜주면 됩니다.
     public void SetPPC(bool a , float b = 0f)
     {
         StartCoroutine(SetPPCDelay(a, b));
@@ -37,20 +39,25 @@ public class DisplayManager : MonoBehaviour
         _pixelPerfectCamera.enabled = a;
     }
 
+    //보스 레이드 연출때 미니맵,hp바 안보이게 하기
     public void HubFadeOut()
     {
         _uiAnimator.SetTrigger(_hubOut);
         Debug.Log("HUB 끄기");
     }
 
+    //hub 키기
     public void HubFadeIn()
     {
         _uiAnimator.SetTrigger(_hubIn);
         Debug.Log("HUB 켜기");
     }
 
+    // 클리어쪽 모션 미추가 - 보스 클리어후,호출
+    // 별도로 보스시작 연출처럼 3~4초정도 보스 기준으로 카메라 잡아주는 연출도 같이 넣어두면 좋을거같음
     public void PlayClearEffect()
     {
+        StartCoroutine(SetPPCDelay(false, 0f));
         StartCoroutine(ClearCrt());
     }
 
